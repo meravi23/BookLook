e37aa70db1f135b864d351b30ffb67522868e961
Merav <merav.mi@gmail.com>
2019-08-19 10:00:05 +0300

    social share - added whatsapp, removed share service

--- app/home/usedBookSearchCtrl.js
+++ app/home/usedBookSearchCtrl.js
@@ -19,7 +19,7 @@ app.controller("usedBookSearchCtrl", function ($scope, bookSrv, $log, $rootScope
     $scope.noShops = false;
     $scope.bookPostsAlreadyRetrievedOnce = false;
     $scope.fieldToSearch = "";
-    $scope.shareMessage = "רציתי לשתף איתך את הספר הזה שמוצע למכירה ב-BookLook!";
+    $scope.shareMessage = "רציתי לשתף איתך את הספר הזה שמוצע למכירה ב-BookLook: ";
     $scope.shareUrl = "https://www.booklookil.com";
 
 
